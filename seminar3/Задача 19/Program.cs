5227c727bf444b52e146218e0e52758149ee3094
vitalii <gilbertus722@yandex.ru>
2022-09-09 18:49:14 +0300

    Че этой заразе не хватает

--- seminar3/Задача 19/Program.cs
+++ seminar3/Задача 19/Program.cs
@@ -1,2 +1,18 @@
 ﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 //Читается число как справа так и слева одинаково
+
+
+Console.Write("Проверяем число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Число: {number} - палиндром.");
+  }
+  else Console.WriteLine($ "Число: {number} - Не палиндром.");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Жду правильное число");
