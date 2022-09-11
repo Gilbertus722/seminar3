bb75dc741828d18af19ae35b8f6eeb92cdaa2240
vitalii <gilbertus722@yandex.ru>
2022-09-11 14:39:15 +0300

    Кубик в кубе

--- seminar3/Задача 23/Program.cs
+++ seminar3/Задача 23/Program.cs
@@ -1,4 +1,5 @@
-﻿//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
@@ -11,3 +12,15 @@ void Cube(int[] cube){
     cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
     counter++;
   }
+}
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
