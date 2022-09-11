9d87049852498cd92a60364feb98b121f553fa20
vitalii <gilbertus722@yandex.ru>
2022-09-11 11:31:52 +0300

    пробую

--- seminar3/Задача 21/Program.cs
+++ seminar3/Задача 21/Program.cs
@@ -15,4 +15,5 @@ int z2 = Coordinate ("z", "B");
  double Decision (double x1, double x2, double y1, double y2, double z1, double z2) {
     return Math.Sqrt(Math.Pow((x2-x1), 2) + (Math.Pow((y2-y1), 2) + (Math.Pow((z2-z1), 2)); 
  }
- double sigmentLength = match.Round (Decision (x1, x2, y1, y2, z1, z2), 2)
+ double sigmentLength = match.Round (Decision (x1, x2, y1, y2, z1, z2), 2);
+ Console.WriteLine ($"отрезок длинной {sigmentLength}");
