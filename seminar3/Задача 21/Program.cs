3e087490a394cc88041dfdf2e95da5ea78c8e215
vitalii <gilbertus722@yandex.ru>
2022-09-11 08:08:44 +0300

    координаты

--- seminar3/Задача 21/Program.cs
+++ seminar3/Задача 21/Program.cs
@@ -1,9 +1,10 @@
 ﻿// Напишите прогу которая берет две точки, а на выходу получаем растояние в 3Д
 
 Console.WriteLine();
-int x1
-int y1
-int z1
-int x2
-int y2
-int z2
+int x1 = Coordinate ("x", "A");
+int y1 = Coordinate ("y", "A");
+int z1 = Coordinate ("z", "A");
+int x2 = Coordinate ("x", "B");
+int y2 = Coordinate ("y", "B");
+int z2 = Coordinate ("z", "B");
+ int coordinate (string )
