629ebb59128f49d8d03fa76f9e73f35298bff5d4
vitalii <gilbertus722@yandex.ru>
2022-09-11 11:35:31 +0300

    ошибки

--- seminar3/Задача 21/Program.cs
+++ seminar3/Задача 21/Program.cs
@@ -7,13 +7,13 @@ int z1 = Coordinate ("z", "A");
 int x2 = Coordinate ("x", "B");
 int y2 = Coordinate ("y", "B");
 int z2 = Coordinate ("z", "B");
- int coordinate (string coorName, string pointName)
+ int coordinate(string coorName, string pointName)
  {
-    ConsoleWrite($ "Координата {coorName} точки {pointName}: ");
+    ConsoleWrite($"Координата {coorName} точки {pointName}: ");
     return Convert.ToInt16(Console.ReadLine());
  }
  double Decision (double x1, double x2, double y1, double y2, double z1, double z2) {
-    return Math.Sqrt(Math.Pow((x2-x1), 2) + (Math.Pow((y2-y1), 2) + (Math.Pow((z2-z1), 2)); 
+    return Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2)); 
  }
  double sigmentLength = match.Round (Decision (x1, x2, y1, y2, z1, z2), 2);
  Console.WriteLine ($"отрезок длинной {sigmentLength}");
